2ae154a5403e609329358e727328f8da4d6ed88f
Dev Codersgarden <vruti.codershut@gmail.com>
2024-12-19 14:29:36 +0530

    migration autoload code added

--- src/RoleServiceProvider.php
+++ src/RoleServiceProvider.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Codersgarden\RoleAssign;
 
@@ -8,23 +8,46 @@ use Illuminate\Support\Facades\Artisan;
 
 class RoleServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-
-        include_once __DIR__.'/Helpers/custom_helpers.php';
-
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-
-        $this->loadViewsFrom(__DIR__.'/views', 'roleassign');
-
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-
-    
-    }
 
     public function register()
     {
         //
     }
 
+    public function boot()
+    {
+
+        include_once __DIR__ . '/Helpers/custom_helpers.php';
+
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'roleassign');
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishMigrations();
+            $this->runSeeder();
+        }
+    }
+
+
+
+    protected function publishMigrations()
+    {
+        $this->publishes([
+            __DIR__ . '/database/migrations' => database_path('migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Run the database seeder.
+     */
+    protected function runSeeder()
+    {
+        if (Artisan::output()) {
+            // Run the seeder automatically
+            Artisan::call('db:seed', ['--class' => 'Codersgarden\\RoleAssign\\database\\seeders\\DatabaseSeeder']);
+        }
+    }
 }
